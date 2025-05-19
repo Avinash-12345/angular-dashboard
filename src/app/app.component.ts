b607b0589daa673803ed5be905d9e6c5d230bf96
Avinash-12345 <avinash.gara@gmail.com>
2025-05-19 18:56:46 +0530

    dynamic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from "./footer/footer.component";
 import { TrafficsComponent } from "./dashboard/traffics/traffics.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, TrafficsComponent, TicketsComponent, ServerStatusComponent],
+  imports: [HeaderComponent, FooterComponent, TrafficsComponent, TicketsComponent, ServerStatusComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/dashboard/traffics/traffics.component.ts
+++ src/app/dashboard/traffics/traffics.component.ts
@@ -8,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './traffics.component.css'
 })
 export class TrafficsComponent {
-
+  dummyTrafficData = [
+     {
+      id: 'd1',
+      value: 433
+     },
+     {
+      id: 'd2',
+      value: 434
+     },
+     {
+      id: 'd3',
+      value: 435
+     },
+     {
+      id: 'd4',
+      value: 436
+     },
+     {
+      id: 'd5',
+      value: 437
+     },
+     {
+      id: 'd6',
+      value: 438
+     }
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data)=> data.value))
 }
